9bfc9aa98535d9fa91f4caf15712544779ad1693
Ben Helmer <Atomox@Bens-MBP-2.fios-router.home>
2016-01-02 11:40:39 -0500

    author: atomox -- NO TICKET -- Inital commit of existing Koken + theme.

---
+++ koken/storage/configuration/database.php
@@ -0,0 +1,9 @@
+<?php
+	return array(
+		'hostname' => '127.0.0.1',
+		'database' => 'benhelmer',
+		'username' => 'benhelmer',
+		'password' => 'benhelmer',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
---
+++ koken/storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return '6c03e7a799cc5cefdee0d71245ac99a9';
