ffcc3cb4a42741a1070ea0f550bf87981c19fc2f
Ben Helmer <Atomox@Bens-MacBook-Pro-2.local>
2016-02-11 22:57:57 -0500

    author: Atomox -- Configuration -- Switching for database creds for Local vs PROD.

--- storage/configuration/database.php
+++ storage/configuration/database.php
@@ -1,11 +1,29 @@
 <?php
 
-	$KOKEN_DATABASE = array(
-		'driver' => 'mysqli',
-		'hostname' => 'internal-db.s166727.gridserver.com',
-		'database' => 'db166727_koken',
-		'username' => 'db166727',
-		'password' => 'Koken16572i657zrqt',
-		'prefix' => 'koken_',
-		'socket' => ''
-	);
+  switch($_SERVER['HTTP_HOST']) {
+
+    // Local development.
+    case 'local.benhelmer.com':
+      $KOKEN_DATABASE = array(
+        'driver' => 'mysqli',
+        'hostname' => 'localhost',
+        'database' => 'db166727_koken',
+        'username' => 'db166727',
+        'password' => 'Koken16572i657zrqt',
+        'prefix' => 'koken_',
+        'socket' => ''
+      );
+      break;
+
+    // PROD
+    default:
+      $KOKEN_DATABASE = array(
+        'driver' => 'mysqli',
+        'hostname' => 'internal-db.s166727.gridserver.com',
+        'database' => 'db166727_koken',
+        'username' => 'db166727',
+        'password' => 'Koken16572i657zrqt',
+        'prefix' => 'koken_',
+        'socket' => ''
+      );
+  }
